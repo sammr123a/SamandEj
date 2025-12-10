76ed1923dd639f21483ccbe1521ed8f645dbb0cf
sammr123a <b2029stkaustria@pshs.edu.ph>
2025-12-11 05:10:00 +0800

    cds&theirlinks

---
+++ public/discoverjs.js
@@ -0,0 +1,64 @@
+var abbeylink = document.getElementById("abbey");
+var neverlink= document.getElementById("nevermind");
+var circuslink= document.getElementById("circus");
+var everybodylink= document.getElementById("everybody");
+var medisinalink= document.getElementById("medisina");
+var noontimelink= document.getElementById("noontime");
+var link24k= document.getElementById("24kmagic");
+var strangerlink= document.getElementById("stranger");
+var kitchielink= document.getElementById("kitchie");
+var moon88link= document.getElementById("moon88");
+var lovedelxlink= document.getElementById("lovedelx");
+var brandnewlink= document.getElementById("brandnew");
+
+abbeylink.onclick = function() {
+  window.open ("https://open.spotify.com/album/0ETFjACtuP2ADo6LFhL6HN?si=vRgefVq_TIOytpSFfVjiVw", "_blank");
+}
+
+neverlink.onclick = function() {
+  window.open ("https://open.spotify.com/album/2UJcKiJxNryhL050F5Z1Fk?si=M-ZJYYoASzaOhsMsotefmw", "_blank");
+}
+
+circuslink.onclick = function() {
+  window.open ("https://open.spotify.com/album/1IalzI8USuu4PB679HVV5C?si=96DIWlo_Rh--cEZ_URo3pg", "_blank");
+}
+
+everybodylink.onclick = function() {
+  window.open ("https://open.spotify.com/artist/7t0rwkOPGlDPEhaOcVtOt9?si=iLZzPXeKTOa-zM-vGnZ0Fw", "_blank");
+}
+
+medisinalink.onclick = function() {
+  window.open ("https://open.spotify.com/album/7sAp0E6Q4dqnrwWkpytEaN?si=ULBe3KoBSIunhl27hJ-HTA", "_blank");
+}
+
+noontimelink.onclick = function() {
+  window.open ("https://open.spotify.com/album/7jEJsX7LeqOxDF9oIcqE5H?si=IcSrgvm6SFOPNpO_tNoRbQ", "_blank");
+}
+
+link24k.onclick = function() {
+  window.open ("https://open.spotify.com/album/4PgleR09JVnm3zY1fW3XBA?si=4xyF8zD2T-SyutzpbAUtpQ", "_blank");
+}
+
+strangerlink.onclick = function() {
+  window.open ("https://open.spotify.com/album/3IILMjMMnoN2sKzgesX8KV?si=4IQ3IE0mQ6CmzKObEm6u6w", "_blank");
+}
+
+kitchielink.onclick = function() {
+  window.open ("https://open.spotify.com/album/7beVKW0o7iRoM0eRT3kGfk?si=La6B3TYqQb2fTRzp1EwesA", "_blank");
+}
+
+moon88link.onclick = function() {
+  window.open ("https://open.spotify.com/album/5h7SARmqKueZqYztB10nhx?si=OzoLyp-DSmSRTKJliR_m9g", "_blank");
+}
+
+lovedelxlink.onclick = function() {
+  window.open ("https://open.spotify.com/album/2PfGKHtqEX58bHtkQxJnWG?si=DH2UWwowRVKd4zGqjuySEg", "_blank");
+}
+
+brandnewlink.onclick = function() {
+  window.open ("https://open.spotify.com/album/27UqZoE1kV6sIV6uQcI28A?si=c8E1EUztREGCj_4vSr2SJA", "_blank");
+}
+
+
+
+document.getElementById('year').textContent = new Date().getFullYear()
